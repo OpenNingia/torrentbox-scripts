20e5b12d3e23645aa49fc98288c396a199f699b2
Daniele Simonetti <oppifjellet@gmail.com>
2015-10-21 06:25:40 +0000

    read multiple subtitles tracks

--- srtextract.py
+++ srtextract.py
@@ -10,16 +10,24 @@ def get_subtitles_track(file_path):
 
 def extract_srt(file_path):
 
-    print('\nprocessing {}'.format(os.path.basename(file_path)))
+    print('processing {}'.format(os.path.basename(file_path)))
     
     ass_path = os.path.splitext(file_path) [0] + ".ass"
     srt_path = os.path.splitext(file_path) [0] + ".srt"
 
+    if os.path.exists(srt_path):
+        print('> srt already exists')
+        return
+
     if not os.path.exists(ass_path):
         extract_ass(file_path, ass_path)
 
-    if os.path.exists(ass_path) and not os.path.exists(srt_path):
+    if os.path.exists(ass_path):
         convert_ass_to_srt(ass_path, srt_path)
+        try:
+            os.remove(ass_path)
+        except:
+            pass
     	print('> srt extraction OK')
     
 def extract_ass(file_path, ass_path):
@@ -30,15 +38,17 @@ def extract_ass(file_path, ass_path):
     if trackns == None or trackns == "":
         print('> no subtitles track found.')
         return
+    track_list = trackns.split()
+
+    for t in track_list:
+        try:    
+            trackn = int(t) - 1
+        except:
+            print('> subtitles tracks incorrect: {}'.format(trackn))
+            return
     
-    try:    
-        trackn = int(trackns) - 1
-    except:
-        print('> subtitles tracks incorrect: {}'.format(trackns))
-        return
-    
-    print('> extract subtitles from track {}'.format(trackn))
-    sout, serr = Popen(['mkvextract', 'tracks', file_path, "{}:{}".format(trackn, ass_path)], stdout=PIPE, stderr=PIPE).communicate()
+        print('> extract subtitles from track {}'.format(trackn))
+        sout, serr = Popen(['mkvextract', 'tracks', file_path, "{}:{}".format(trackn, ass_path)], stdout=PIPE, stderr=PIPE).communicate()
     
     # print(serr)
 
